049baeb5587690dc5b89b952d58477e67bbeb331
Dimedrolity <dima.redkin.1999@gmail.com>
2020-09-09 16:00:23 +0500

    изменил самописные view на generic views - ListView и DetailView

--- polls/views.py
+++ polls/views.py
@@ -1,28 +1,48 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-
-    return render(request, 'polls/index.html', context={
-        'latest_question_list': latest_question_list
-    })
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, 'polls/detail.html', context={
-        'question': question
-    })
-
-
-def results(request, question_id):
-    return HttpResponse(f' results of q = {question_id}')
+from .models import Question, Choice
 
 
 def vote(request, question_id):
-    return HttpResponse(f'voting for q = {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/question_detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/question_list.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    # By default, the DetailView generic view uses a template called
+    # <app name>/<model name>_detail.html.
+    # думаю, лучше указать явно
+
+    template_name = 'polls/question_detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/question_results.html'
