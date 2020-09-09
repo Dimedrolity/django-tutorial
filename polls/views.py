2d69b177ab1e89769c02b1ebc72abe8aa40bbe8f
Dimedrolity <dima.redkin.1999@gmail.com>
2020-09-09 13:05:09 +0500

    добавил 2 темплейта - вывод последних 5 вопросов и конкретный вопрос.

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+
+    return render(request, 'polls/index.html', context={
+        'latest_question_list': latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/detail.html', context={
+        'question': question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f' results of q = {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'voting for q = {question_id}')
